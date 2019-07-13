b7e237b8e681c5d4079a48d58b3b0ea47f0e334e
Uberacy Tottoli <bera@boldtech.com.br>
2019-07-13 11:20:31 -0300

    Add register api call from angular

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -9,15 +9,18 @@ export class AuthService {
 baseUrl = 'http://localhost:5000/api/auth/';
 constructor(private http: HttpClient) { }
 
-login(model: any) {
-  return this.http.post(this.baseUrl + 'login', model).pipe(
-    map((response: any ) => {
-      const user = response;
-      if (user) {
-        // Add token to local storage "Client"
-        localStorage.setItem('token', user.token);
-      }
-    })
-  );
-}
+  login(model: any) {
+      return this.http.post(this.baseUrl + 'login', model).pipe(
+        map((response: any ) => {
+          const user = response;
+          if (user) {
+            // Add token to local storage "Client"
+            localStorage.setItem('token', user.token);
+          }
+        })
+      );
+    }
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +11,16 @@ export class RegisterComponent implements OnInit {
   // Output creates events, so we have to create a event emitter
   @Output() cancelRegister = new EventEmitter();
   model: any = { };
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Registration successful');
+    }, error => {
+      console.log(error);
+    });
   }
   cancel() {
     this.cancelRegister.emit(false);
